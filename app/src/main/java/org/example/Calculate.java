69626982678fe6ddfbbd6fe3a92d10e49c0d141f
Hayate-Hirakawa-ac <m1m02530150@oit.ac.jp>
2025-07-07 01:38:07 +0900

    課題1

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,32 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public double average(int x, int y) {
+    return (double) (x + y) / 2;
+  }
+
+  public int siguma(int x, int y) {
+    int s = 0;
+    for (int i = x; i <= y; i++) {
+      s += i;
+    }
+    return s;
+  }
+
+  public int[] evenNumber_oddNumber(int x, int y) {
+    int even = 0;
+    int odd = 0;
+    int data[] = new int[2];
+    for (int i = x; i <= y; i++) {
+      if (i % 2 == 0) {
+        even += i;
+      } else {
+        odd += i;
+      }
+      data[0] = even;
+      data[1] = odd;
+    }
+    return data;
+  }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -10,4 +10,25 @@ public class CalculateTest {
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
+
+  @Test
+  public void testAverage() {
+    Calculate calculate = new Calculate();
+    double expected = 2.5;
+    assertEquals(expected, calculate.average(2, 3), 0);
+  }
+
+  @Test
+  public void testSiguma() {
+    Calculate calculate = new Calculate();
+    int expected = 55;
+    assertEquals(expected, calculate.siguma(1, 10));
+  }
+
+  @Test
+  public void testEvenNumber_oddNumber() {
+    Calculate calculate = new Calculate();
+    int[] expected = { 30, 25 };
+    assertArrayEquals(expected, calculate.evenNumber_oddNumber(1, 10));
+  }
 }
